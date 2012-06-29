5397761d775685e86a651597fb6c9c3eaa83f6c6
Kumar McMillan <kumar.mcmillan@gmail.com>
2012-06-29 13:31:49 -0500

    Updates store URL

--- www/js/app.js
+++ www/js/app.js
@@ -1,6 +1,6 @@
 (function() {
     mozmarket.receipts.Prompter({
-        storeURL: "https://marketplace.mozilla.org/app/myapp",
+        storeURL: "https://marketplace-dev.allizom.org/en-US/app/private-yacht/",
         supportHTML: '<a href="mailto:kumar.mcmillan@gmail.com">email kumar.mcmillan@gmail.com</a>',
         verify: true
     });
