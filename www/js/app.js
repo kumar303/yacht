e50f4a3409bf2dc1385d1c23db684c72890e43d7
Kumar McMillan <kumar.mcmillan@gmail.com>
2012-06-29 16:56:26 -0500

    Make prompter accessible for debugging

--- www/js/app.js
+++ www/js/app.js
@@ -1,5 +1,5 @@
 (function() {
-    mozmarket.receipts.Prompter({
+    installPrompt = mozmarket.receipts.Prompter({
         storeURL: "https://marketplace-dev.allizom.org/en-US/app/private-yacht/",
         supportHTML: '<a href="mailto:kumar.mcmillan@gmail.com">email kumar.mcmillan@gmail.com</a>',
         verify: true,
