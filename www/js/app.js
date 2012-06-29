13a5badd1761dca87794a4a9d1bc5001a7a201d5
Kumar McMillan <kumar.mcmillan@gmail.com>
2012-06-29 16:50:51 -0500

    Add logging

--- www/js/app.js
+++ www/js/app.js
@@ -2,6 +2,7 @@
     mozmarket.receipts.Prompter({
         storeURL: "https://marketplace-dev.allizom.org/en-US/app/private-yacht/",
         supportHTML: '<a href="mailto:kumar.mcmillan@gmail.com">email kumar.mcmillan@gmail.com</a>',
-        verify: true
+        verify: true,
+        verifierOptions: {onlog: mozmarket.receipts.Verifier.consoleLogger}
     });
 })();
