bfd222c5d6b2fef0f5a902bd7e98a8945edb8a3a
Kumar McMillan <kumar.mcmillan@gmail.com>
2012-06-29 12:44:34 -0500

    It's a private yacht!

---
+++ www/js/app.js
@@ -0,0 +1,7 @@
+(function() {
+    mozmarket.receipts.Prompter({
+        storeURL: "https://marketplace.mozilla.org/app/myapp",
+        supportHTML: '<a href="mailto:kumar.mcmillan@gmail.com">email kumar.mcmillan@gmail.com</a>',
+        verify: true
+    });
+})();
